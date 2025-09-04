4a50648c38081ef5a9dff8dfc9a855e8e4575f8e
nipunrawat123 <nipunrawat0906@gmail.com>
2025-09-04 16:10:19 +0530

    [Nipun] Add : Added divisible by 5

---
+++ Control Flow/Level-1/Divisible.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Divisible{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Take input
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        // Check divisibility
+        if (num % 5 == 0) {
+            System.out.println(num + " is divisible by 5.");
+        } else {
+            System.out.println(num + " is not divisible by 5.");
+        }
+
+        sc.close();
+    }
+}
